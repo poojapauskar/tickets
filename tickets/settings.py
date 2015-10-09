6ddacb66adbbefeb3667819173269f1d9f4b40a7
poojapauskar <git.poojapauskar@gmail.com>
2015-10-09 15:01:30 +0530

    initial commit

--- tickets/settings.py
+++ tickets/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # # Parse database configuration from $DATABASE_URL
-# import dj_database_url
-# DATABASES['default'] =  dj_database_url.config()
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
