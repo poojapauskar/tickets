4f9bd9778472aca89fe8f76c98da620e3e2632b6
poojapauskar <git.poojapauskar@gmail.com>
2015-10-09 12:20:26 +0530

    initial commit

--- tickets/wsgi.py
+++ tickets/wsgi.py
@@ -11,6 +11,13 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tickets.settings")
 
-application = get_wsgi_application()
+from dj_static import Cling
+
+
+
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tickets.settings")
+application = Cling(get_wsgi_application())
+#application = get_wsgi_application()
