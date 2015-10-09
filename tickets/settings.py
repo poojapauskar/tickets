02170a57957db13ff7a070f169c3585171a8c8aa
poojapauskar <git.poojapauskar@gmail.com>
2015-10-09 15:49:37 +0530

    initial commit

--- tickets/settings.py
+++ tickets/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.XFrameOptionsMiddleware',
-    'django.middleware.clickjacking.
+    'django.middleware.clickjacking,
     'django.middleware.security.SecurityMiddleware',
 )
 
